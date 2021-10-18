5b5a69717b6ae6b03978985c66dc27464a1d4ab9
bee2805 <200089@virtualwindow.co.za>
2021-10-18 13:59:20 +0200

    Movie Library html + js update
    I just updated the html and js so that it's more neat and easy to read

--- js/MovieLibrary.js
+++ js/MovieLibrary.js
@@ -3,32 +3,40 @@ $(document).ready(function() {
     const base_url = 'https://api.themoviedb.org/3';
     const api_key = '?api_key=4d51cad1f6d8c339de5671b9d1223770';
     const img_url = 'https://image.tmdb.org/t/p/w300/'
-    const url = base_url + '/movie/popular' + api_key;
+    const popular_url = '/movie/popular';
+    const genre_url = '/genre/movie/list';
+    const url = base_url + popular_url + api_key;
+
+    console.log(base_url + genre_url + api_key);
+    console.log(url);
+
 
     $.getJSON(url, function(data){
         console.log(data);
 
         for(i = 0 ; i < 20; i++){
 
-            $(".action-movies").append(
+            if(data.results[i].genre_ids[0] === 28){
 
-                `<div class="col-lg-3 col-md-4 item">
-                    <div class="card">
-                        <div class="img_con">
-                            <img src="${img_url+data.results[i].poster_path}" alt="">
-                        </div>
-                        <div class="card-body">
-                            <h5 class="movie-title">${data.results[i].title}</h5>
-                            <div class="side-buttons">
-                                <div class="movie-dropdown button"></div>
-                                <div class="add button"></div>
-                                <div class="like button"></div>
+                $(".action-movies").append(
+
+                    `<div class="col-lg-3 col-md-4 item">
+                        <div class="card">
+                            <div class="img_con">
+                                <img src="${img_url+data.results[i].poster_path}" alt="">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="movie-title">${data.results[i].title}</h5>
+                                <div class="side-buttons">
+                                    <div class="like button"></div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>`
+                    </div>`
+                );
 
-            );
+            };
+
         };
     })
     
