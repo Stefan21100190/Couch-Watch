6213ca2463f8d84b9cbe4401481e8d9ca652fb01
bee2805 <200089@virtualwindow.co.za>
2021-10-28 18:24:09 +0200

    Removed random functions
    removed random function on movieLibrary.js

--- js/MovieLibrary.js
+++ js/MovieLibrary.js
@@ -8,100 +8,6 @@ $(document).ready(function() {
     const url = base_url + popular_url + api_key;
     const topRatedUrl = base_url + '/movie/top_rated' + api_key;
 
-    const genreEl = document.getElementById('genre');
-
-    const genres = {
-        "genres": [
-        {
-        "id": 28,
-        "name": "Action"
-        },
-        {
-        "id": 12,
-        "name": "Adventure"
-        },
-        {
-        "id": 16,
-        "name": "Animation"
-        },
-        {
-        "id": 35,
-        "name": "Comedy"
-        },
-        {
-        "id": 80,
-        "name": "Crime"
-        },
-        {
-        "id": 99,
-        "name": "Documentary"
-        },
-        {
-        "id": 18,
-        "name": "Drama"
-        },
-        {
-        "id": 10751,
-        "name": "Family"
-        },
-        {
-        "id": 14,
-        "name": "Fantasy"
-        },
-        {
-        "id": 36,
-        "name": "History"
-        },
-        {
-        "id": 27,
-        "name": "Horror"
-        },
-        {
-        "id": 10402,
-        "name": "Music"
-        },
-        {
-        "id": 9648,
-        "name": "Mystery"
-        },
-        {
-        "id": 10749,
-        "name": "Romance"
-        },
-        {
-        "id": 878,
-        "name": "Science Fiction"
-        },
-        {
-        "id": 10770,
-        "name": "TV Movie"
-        },
-        {
-        "id": 53,
-        "name": "Thriller"
-        },
-        {
-        "id": 10752,
-        "name": "War"
-        },
-        {
-        "id": 37,
-        "name": "Western"
-        }
-        ]
-    }
-
-    setGenre()
-
-    function setGenre() {
-        genreEl.innerHTML = ``;
-        genres.forEach(genre => {
-            const t = document.createElement('option');
-            t.classList.add('tag')
-        })
-    }
-
-
     $.getJSON(url, function(data){
         console.log(data);
 
@@ -200,7 +106,5 @@ $(document).ready(function() {
     })
     
 
-    
-
     return full_html;
-}
+})
